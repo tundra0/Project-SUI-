850fc9b68f86b555c863d31df2c75d90385e9c3d
tundra0 <51240452+tundra0@users.noreply.github.com>
2019-06-15 19:13:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -99,7 +99,7 @@ async def on_message(message):
     
     if (message.content.endswith(('よ','かぁ')) and random.random() > 0.6):
         await message.channel.send(resp)
-    elif message.content == 'おはよう'
+    elif message.content == 'おはよう':
         pass
         
 #役職付与
