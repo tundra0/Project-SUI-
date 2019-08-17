9150eae34c13a584cc3c05ef1994e69436221e43
tundra0 <51240452+tundra0@users.noreply.github.com>
2019-08-17 21:24:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,8 +29,7 @@ async def on_message(message):
 #サーバーステータス
  #サーバー人数
     if '翠、今のサーバー人数を教えて' in message.content:
-        S = len(guild.members)
-        embed = discord.Embed(title=サーバー人数, description=S,color=discord.Color.green())
+        embed = discord.Embed(title=サーバー人数, description=len(guild.members),color=discord.Color.green())
         await message.channel.send(embed=embed)
 
 #会話
