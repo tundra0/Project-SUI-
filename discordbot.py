2fc2bcce1dd01556f9aa69a40b07279595bb3db5
tundra0 <51240452+tundra0@users.noreply.github.com>
2019-08-18 21:14:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,7 +29,7 @@ async def on_message(message):
 #サーバーステータス
  #サーバー人数
     if '翠、今のサーバーステータスを教えて' in message.content:
-        await message.channel.send(f'現在就寝中のノイマンちゃんに代わり翠がお伝えします。今のサーバー人数は{len(message.guild.members)}人、ユーザー総数は{len(client.users)}です！')
+        await message.channel.send(f'現在就寝中のノイマンちゃんに代わり翠がお伝えします。今のサーバー人数は{len(message.guild.members)}人です！')
 
 #会話
  #おはよう
