cc3d8cd6c65d90faf978650b973c6c9f8d304031
TimeRainTime <51240452+tundra0@users.noreply.github.com>
2020-02-18 17:55:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -128,41 +128,6 @@ async def on_message(message):
     elif (message.content.endswith(('よ','かぁ')) and random.random() < 0.4):
         await message.channel.send(resp)
 
-#リアクション追加式役職付与システム
-
-@client.event
-async def on_raw_reaction_add(payload):
-
-    if payload.message_id == ID:
-
-        print(payload.emoji.name)
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-
-        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
-
-        if role is not None:
-            print(role.name + " was found!")
-            print(role.id)
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            await member.add_roles(role)
-            print("done")
-
-
-@client.event
-async def on_raw_reaction_remove(payload):
-    if payload.message_id == ID:
-        print(payload.emoji.name)
-
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
-        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
-
-        if role is not None:
-            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
-            await member.remove_roles(role)
-            print("done")
-
 #ウェルカムメッセージ
 
 get_channel_id = {673412098683830284:673412099350855702,633956743616921620:634257472865173525}
